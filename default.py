933ee112fa5c1d55d39438e624b18119f6ed342d
Russell Gapasin <russell@fanswifi.com>
2020-06-05 19:26:49 +0800

    Refactored code with pandas

--- default.py
+++ default.py
@@ -1,10 +1,8 @@
-import os
-import csv
 import requests
-import numpy as np
+import pandas as pd
 from bs4 import BeautifulSoup
 
-def scrape():
+def main():
     html_page = requests.get('http://quotes.toscrape.com/')
     content = html_page.content
     soup = BeautifulSoup(content, 'html.parser')
@@ -15,22 +13,11 @@ def scrape():
     quotes = [quote.text for quote in quote_elements]
     authors = [author.text for author in author_elements]
 
-    quote_data = np.column_stack((quotes, authors))
-
-    return quote_data
-
-def storeToCsv(quotes):
-    header = ['quotes', 'authors']
-    with open('default.csv', 'w') as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(header)
-        csvwriter.writerows(quotes)
-
-def main():
-    # step 1: scrape website
-    quotes = scrape()
-    # step 2: store data to csv file
-    storeToCsv(quotes)
+    quote_data = pd.DataFrame({
+        'quotes': quotes,
+        'authors': authors
+    })
+    quote_data.to_csv('default.csv', index=False)
 
 if __name__ == '__main__':
     main()
