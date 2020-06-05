f7de0a8acc61d0550743bcf54969d7aa412c75c0
Russell Gapasin <russell@fanswifi.com>
2020-06-05 20:16:39 +0800

    Modified script to scrape multiple pages

--- default.py
+++ default.py
@@ -3,21 +3,28 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 def main():
-    html_page = requests.get('http://quotes.toscrape.com/')
-    content = html_page.content
-    soup = BeautifulSoup(content, 'html.parser')
+    all_quotes = []
+    all_authors = []
 
-    quote_elements = soup.find_all('span', class_='text')
-    author_elements = soup.find_all('small', class_='author')
-    
-    quotes = [quote.text for quote in quote_elements]
-    authors = [author.text for author in author_elements]
+    for i in range(1,11):
+        html_page = requests.get(f'http://quotes.toscrape.com/page/{i}/')
+        content = html_page.content
+        soup = BeautifulSoup(content, 'html.parser')
+
+        quote_elements = soup.find_all('span', class_='text')
+        author_elements = soup.find_all('small', class_='author')
+
+        quotes = [quote.text for quote in quote_elements]
+        authors = [author.text for author in author_elements]
+
+        all_quotes.extend(quotes)
+        all_authors.extend(authors)
 
     quote_data = pd.DataFrame({
-        'quotes': quotes,
-        'authors': authors
+        'quotes': all_quotes,
+        'authors': all_authors
     })
-    quote_data.to_csv('default.csv', index=False)
+    quote_data.to_csv('csv/default.csv', index=False)
 
 if __name__ == '__main__':
     main()
